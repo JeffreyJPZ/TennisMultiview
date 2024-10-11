f6252a823e261bc8c71f87a06e34fa81185bf640
Jeffrey Zhang <jjpzhang@students.cs.ubc.ca>
2024-10-11 00:19:01 -0700

    Initialize project

---
+++ TennisMultiview/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace TennisMultiview
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+    }
+}
